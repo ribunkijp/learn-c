d90768028660b15be2012eba27b9235b8392d35a
ribunki <lwh1194@gmail.com>
2025-04-15 10:00:37 +0900

    c

--- 1.c
+++ 1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("hello\n%12f", 2025.0);
-}
 
+
+int main(void) {
+	/*int width, height;
+
+	width = 1;
+	height = (width = width *2 + 1);
+
+	int x, y, z, m, n;
+	x = y = z = m = n = 2;*/
+
+	/*for (int g = 0; g < 10; g++) {
+		printf("%d\n", g);
+	}*/
+
+	// printf("%d %d", height, z);
+	float a = 6.0 / 5;
+	printf("%*.*f", 5, 3, a);
+}
