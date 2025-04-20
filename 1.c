dc0c75c05b9d9aff51f269e170ea4dae3e4cab9e
ribunki <lwh1194@gmail.com>
2025-04-20 16:12:47 +0900

    進数

--- 1.c
+++ 1.c
@@ -1,4 +1,31 @@
 ﻿#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+//int binary_to_decimal(const char* binary) {
+//    int decimal = 0;
+//    int length = strlen(binary);  // 文字列の長さを取得
+//
+//    // 文字列を1ビットずつ評価して10進数に変換
+//    for (int i = 0; i < length; i++) {
+//        if (binary[i] == '1') {
+//            decimal += pow(2, length - i - 1);  // 2のべき乗を加算
+//        }
+//    }
+//
+//    return decimal;
+//}
+//
+//int main() {
+//    const char* binary = "101000";  // 2進数の文字列
+//    int decimal = binary_to_decimal(binary);  // 10進数に変換
+//
+//    printf("2進数 %s は 10進数 %d です。\n", binary, decimal);
+//    int a = 21;
+//
+//
+//    return 0;
+//}
 
 void decimal_to_binary(int n) {
 	if (n > 1) {
@@ -12,9 +39,26 @@ int main(void) {
 
 
 	
-	unsigned int num = 123;
-	decimal_to_binary(123);
+	unsigned int num = 10;
+	num = num << 3;
+	decimal_to_binary(num);
 	
 	return 0;
 	
 }
+//void hex_to_binary(unsigned int hex) {
+//    // 32ビットとして表示（上位ビットから）
+//    for (int i = 31; i >= 0; i--) {
+//        printf("%d", (hex >> i) & 1);
+//        if (i % 4 == 0) printf(" ");  // 見やすいように4ビットごとにスペース
+//    }
+//    printf("\n");
+//}
+//
+//int main(void) {
+//    unsigned int hex_value = 0x1A3;  // 例: 16進数の1A3（=10進数419）
+//    printf("Hex: 0x%X\n", hex_value);
+//    printf("Binary: ");
+//    hex_to_binary(hex_value);
+//    return 0;
+//}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # learn-c
 
-###　十進数 to 二進数
+###　10進数 to 2進数
 
-```
+```c
 #include <stdio.h>
 
 void decimal_to_binary(int n) {
@@ -22,6 +22,37 @@ int main(void) {
 	
 	return 0;
 	
+}
+```
+
+### 2進数 to 10進数
+
+```c
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int binary_to_decimal(const char *binary) {
+    int decimal = 0;
+    int length = strlen(binary);  // 文字列の長さを取得
+
+    // 文字列を1ビットずつ評価して10進数に変換
+    for (int i = 0; i < length; i++) {
+        if (binary[i] == '1') {
+            decimal += pow(2, length - i - 1);  // 2のべき乗を加算
+        }
+    }
+    
+    return decimal;
+}
+
+int main() {
+    const char *binary = "101101";  // 2進数の文字列
+    int decimal = binary_to_decimal(binary);  // 10進数に変換
+
+    printf("2進数 %s は 10進数 %d です。\n", binary, decimal);
+
+    return 0;
 }
 ```
 
