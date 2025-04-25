d5fde762c5cafddc1f83638c34fb8096ae72e60e
ribunki <lwh1194@gmail.com>
2025-04-25 09:32:01 +0900

    関数_ポインター

--- 1.c
+++ 1.c
@@ -5,195 +5,21 @@
 #include <limits.h>
 #include<stdint.h>
 
+void swap(int *x, int *y) {
+	int tem;
+	tem = *x;
+	*x = *y;
+	*y = tem;
+}
 
-//int binary_to_decimal(const char* binary) {
-//    int decimal = 0;
-//    int length = strlen(binary);  // 文字列の長さを取得
-//
-//    // 文字列を1ビットずつ評価して10進数に変換
-//    for (int i = 0; i < length; i++) {
-//        if (binary[i] == '1') {
-//            decimal += pow(2, length - i - 1);  // 2のべき乗を加算
-//        }
-//    }
-//
-//    return decimal;
-//}
-//
-//int main() {
-//    const char* binary = "101000";  // 2進数の文字列
-//    int decimal = binary_to_decimal(binary);  // 10進数に変換
-//
-//    printf("2進数 %s は 10進数 %d です。\n", binary, decimal);
-//    int a = 21;
-//
-//
-//    return 0;
-//}
-
-//void decimal_to_binary(int n) {
-//	if (n > 1) {
-//		decimal_to_binary(n / 2);  // 再帰的に2で割り続ける
-//	}
-//	printf("%d", ~n % 2);  // 余りを表示（最下位ビットから順に表示される）
-//}
-//
-//int main(void) {
-	
-
-
-	
-	/*unsigned int num = 10;
-	
-	decimal_to_binary(num);*/
-
-
-	
-		/*unsigned char num = 10;
-		unsigned int result = ~num & 0x0F;
-
-		printf("元の値: %u\n", num);
-		printf("反転後: %u\n", result);*/
-	/*const char* str_val = "0010";
-	unsigned long int dev_val = strtol(str_val, NULL, 2);
-	dev_val = ~dev_val & 0x0F;
-	dev_val = dev_val | 10;
-	printf("%lu\n", dev_val);
-
-	return 0;*/
-	
-	
-	//printf("%d", num);
-	
-	//decimal_to_binary(num);
-	/*num = ~num;
-	printf(num);*/
-	/*int i = 9;
-	if (i > 10) {
-		printf("%s", ">>10");
-	}
-	else {
-		printf("%s", "<<10");
-
-	}*/
-	//int i = 9;
-	//i > 10 ? printf("%s", ">>10") : printf("%s", "<<10");
-
-
-	
-	//return 0;
-	
-//}
-//void hex_to_binary(unsigned int hex) {
-//    // 32ビットとして表示（上位ビットから）
-//    for (int i = 31; i >= 0; i--) {
-//        printf("%d", (hex >> i) & 1);
-//        if (i % 4 == 0) printf(" ");  // 見やすいように4ビットごとにスペース
-//    }
-//    printf("\n");
-//}
-//
-//int main(void) {
-//    unsigned int hex_value = 0x1A3;  // 例: 16進数の1A3（=10進数419）
-//    printf("Hex: 0x%X\n", hex_value);
-//    printf("Binary: ");
-//    hex_to_binary(hex_value);
-//    return 0;
-//}
 int main(void) {
 
-	//int a = 10;
+	int a = 1, b = 2;
 	
-	/*while (a > 10) {
-		
-		printf("%d", a);
-		a--;
-		
-	}*/
-	/*do {
-		printf("%d", a);
-
-	} while (a > 10);
+	swap(&a, &b);
 	
-	return 0;*/
-	/*for (int b = 5; b > 1; b--) {
-		printf("%d", b);
-	}*/
-	/*int a = 10;
+	printf("%d %d", a, b);
 
-	while (a > 0) {
-		a--;
-		if ((a % 2) == 0) continue;
-		
-		printf("%d\n", a);
-	}*/
-	/*int age;
-	char name[50];
 	
-start:
-
-	printf("名前を入力してください：");
-	scanf(" %s", name);
-	printf("年齢を入力してください：");
-	scanf("%d", &age);
-	if (age >= 30) goto start;
-	printf("こんにちは、%sさん\n、%d歳", name, age);*/
-
-	/*char * a = '111';
-	printf("%s", a);*/
-	/*int ch;
-	printf("キーを押してください：\n");
-	ch = getchar();
-	printf("押したキーのASCIIコードは: %d\n", ch);
-	return 0;*/
-	/*char a = '1';
-	char b = 'B';
-
-	printf("%d", a + b);*/
-	//printf("%d\n%d", INT_MAX, INT_MIN);
-
-	/*int a = 0x1A;
-	a = a + 10;
-	printf("%d", a);*/
-
-	/*if (0.1 + 0.2 == 0.3) {
-		printf("true");
-	}
-	else {
-		printf("false");
-	}*/
-
-	//unsigned int a = UINT_MAX;
-	//a++;
-	//a--;
-	//printf("%u", a);
-	/*int x = sizeof(3.14);
-	printf("%zd", x);*/
-	//int val = 1;
-	///*int* x = &val;
-	//*x = 2;
-	//printf("%d", val);*/
-	//if (val == *(&val)) {
-	//	printf("true");
-	//}
-	
-	//int val = 3;
-	//printf("%d\n", val);
-	//p = &val;
-	//*p = 5;
-	//printf("%d", val);
-	int* p = NULL;
-	int val = 3;
-	//int val1 = 4;
-	p = &val;
-	printf("%p\n", &p);
-	printf("%d", *p);
-	/*if (&val > &val1) {
-		printf("true");
-	}
-	else {
-		printf("false");
-	}*/
-
 	return 0;
 }
